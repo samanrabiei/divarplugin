e0740a9a4d20b6ee340adf8c5fc78f54ef4f3660
saman rabiei <80220975+samanrabiei@users.noreply.github.com>
2025-11-19 16:59:32 +0330

    افزودن فایل زبان به برنامه

---
+++ lang/fa/app.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'return to divar' => 'بازگشت به دیوار',
+    'return to services' => 'بازگشت به سرویس ها',
+    'mobile' => 'موبایل',
+    'codemele' => 'کد ملی',
+    'this service requist return' => '+ درخواست دوباره این سرویس',
+    'Mobile number and national ID matching (Shahkar identity verification)' => 'تطابق شماره موبایل و کد ملی (احراز هویت شاهکار)',
+    'Request result' => 'نتیجه درخواست',
+    '' => '',
+];
