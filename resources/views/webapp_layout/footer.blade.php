2d3572acc400d12a07227b729fdb28f5cded3747
saman rabiei <80220975+samanrabiei@users.noreply.github.com>
2025-11-23 11:20:21 +0330

    ویرایش toast

--- resources/views/webapp_layout/footer.blade.php
+++ resources/views/webapp_layout/footer.blade.php
@@ -13,30 +13,31 @@
 <script src="{{ asset('assets/js/custom.js') }}"></script>
 <script src="{{ asset('assets/toast/script.js') }}"></script><!-- toast -->
 <script>
-    @if (session('success'))
+    @if (session('success') && is_array(session('success')))
         openToast('success');
         document.querySelector("#toast-message p").innerText = "{{ session('success')['message'] }}";
         document.querySelector("#toast-message h4").innerText = "{{ session('success')['title'] }}";
     @endif
 
-    @if (session('error'))
+    @if (session('error') && is_array(session('error')))
         openToast('error');
         document.querySelector("#toast-message p").innerText = "{{ session('error')['message'] }}";
         document.querySelector("#toast-message h4").innerText = "{{ session('error')['title'] }}";
     @endif
 
-    @if (session('warning'))
+    @if (session('warning') && is_array(session('warning')))
         openToast('warning');
         document.querySelector("#toast-message p").innerText = "{{ session('warning')['message'] }}";
         document.querySelector("#toast-message h4").innerText = "{{ session('warning')['title'] }}";
     @endif
 
-    @if (session('info'))
+    @if (session('info') && is_array(session('info')))
         openToast('info');
         document.querySelector("#toast-message p").innerText = "{{ session('info')['message'] }}";
         document.querySelector("#toast-message h4").innerText = "{{ session('info')['title'] }}";
     @endif
 </script>
+
 </body>
 
 </html>
