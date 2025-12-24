0e1dfa791f027a16f207af819f89c72c17202cfb
saman rabiei <80220975+samanrabiei@users.noreply.github.com>
2025-12-24 18:46:09 +0330

    افزودن خارج کننده متن و جایگزینی به هلپر

--- app/Helpers/TextHelper.php
+++ app/Helpers/TextHelper.php
@@ -2,14 +2,25 @@
 
 namespace App\Helpers;
 
+use App\Models\Admin\SmsTemplate;
+
 class TextHelper
 {
-    public static function replace($text, array $replacements)
+    public static function replace($key_content, array $replacements)
     {
+        $text = self::get($key_content);
+
         foreach ($replacements as $key => $value) {
             $text = str_replace('{' . $key . '}', $value, $text);
         }
 
         return $text;
+    }
+
+    public static function get(string $key): string
+    {
+        return SmsTemplate::where('key', $key)
+            ->where('is_active', 1)
+            ->value('content') ?? '';
     }
 }
